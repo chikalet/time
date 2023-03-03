254202c46e0fa7f425703872427c17c4db11dcb5
Chikalet <93155754+chikalet@users.noreply.github.com>
2023-03-03 21:09:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<DOCTYPE>
+    <html>
+    <head>
+        <meta charset = “UTF-8”>
+        <title></title>
+    </head>
+    <body>
+    <form action="convert.php" method="post">
+        <input  type="number" name="hour" class="">
+        <input  type="number" name="minute" class="">
+        <button class="btn waves-effect waves-light btnKryptoHeadersell" type="submit" name="action">Подтвердить</button>
+    </form>
+    </body>
+    </html>
+</DOCTYPE>
